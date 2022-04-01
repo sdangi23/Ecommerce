1113bf9a49e36d6f8390ed59df705ccb741a81a9
Shubham Dangi <shubhamdangi@Shubhams-MacBook-Air.local>
2022-04-02 00:22:14 +0530

    Agn2

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
-app.use(shopRoutes);
+app.use('/add-product' , (req,res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title" placeholder="Add Product Name"><input type="number" name="quantity" placeholder="Add Product Quantity"><button type="submit"> Add Product </button></form>');
+})
+
+app.use('/product' , (req,res, next) => {
+    console.log(req.body);
+    res.redirect('/'); 
+})
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Welcome to the Home Page</h1>');
+})
+ 
 
 app.listen(3000);
